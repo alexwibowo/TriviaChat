3cfc442be6be6460dc8e668a5278fdaedf8f238a
Alex Wibowo <alexwibowo@gmail.com>
2020-08-23 12:28:02 +1000

    Fixing the way we present questions. We can pick random question set, but questions in a questionset has to be presented sequentially

--- src/main/java/com/wibowo/games/triviachat/QuestionSet.java
+++ src/main/java/com/wibowo/games/triviachat/QuestionSet.java
@@ -5,11 +5,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 public final class QuestionSet implements Iterable<Question>{
-    public static final Random randomiser = new Random(System.currentTimeMillis());
-
     private final int setNumber;
     @NotNull
     private final List<Question> questions;
@@ -19,22 +16,22 @@ public final class QuestionSet implements Iterable<Question>{
         this.questions = new ArrayList<>();
     }
 
-    public int getSetNumber() {
-        return setNumber;
-    }
-
     public QuestionSet addQuestion(final @NotNull Question question) {
         this.questions.add(question);
         return this;
-    }
-
-    public Question randomQuestion() {
-        return questions.get(randomiser.nextInt(questions.size()));
     }
 
     @NotNull
     @Override
     public Iterator<Question> iterator() {
         return questions.iterator();
+    }
+
+    public Question getQuestion(final int index) {
+        return questions.get(index);
+    }
+
+    public boolean hasMoreQuestion(int index) {
+        return index <= questions.size() - 1;
     }
 }
